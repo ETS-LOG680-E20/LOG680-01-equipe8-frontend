874ab1451c56b049dc08323b895ed59093924bfa
Simon <pelletier.simon.18@gmail.com>
2020-07-23 11:11:20 -0400

    All endpoints implemented

--- app/routes/main.js
+++ app/routes/main.js
@@ -2,16 +2,83 @@ import Route from '@ember/routing/route';
 
 export default class MainRoute extends Route {
   model(){
+    var today = new Date();
+    var lastMonth = new Date();
+    lastMonth.setDate(today.getDate() - 30);
     this.set('defaultDate', 'test');
-    this.set('test7', this.processTimeWithWithIssue(4601));
+    this.set('baseURL', 'https://localhost:44319/metric/');
+    this.set('columns', this.getColumns());
+    this.set('test1', this.processTimeWithIssue(4601));
+    this.set('test2', this.processTimeWithTimeInterval(lastMonth.toDateString(), today.toDateString()));
+    this.set('test3', this.activeIssuesWithColumnId(8520666));
+    this.set('test4', this.completedIssuesWithTimeInterval(lastMonth.toDateString(), today.toDateString()));
+    this.set('test5', this.issuesForEachColumnWithTimeInterval(lastMonth.toDateString(), today.toDateString()));
+    this.set('test5', this.issuesForEachColumnWithTimeInterval(lastMonth.toDateString(), today.toDateString()));
+    this.set('test6', this.openPullRequest());
+    this.set('test7', this.commits());
   }
 
-  processTimeWithWithIssue(id){
-    var url = `https://localhost:44319/metric/ProcessTime/${id}`;
+  getColumns(){
+    var url = `${this.baseURL}Columns`;
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", url, false );
     xmlHttp.send( null );
-    console.log(xmlHttp.responseText);
-    return xmlHttp.responseText;
+    return JSON.parse(xmlHttp.response)['metricValue'];
+  }
+
+  processTimeWithIssue(id){
+    var url = `${this.baseURL}ProcessTime/${id}`;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false );
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.response)['metricValue'];
+  }
+
+  processTimeWithTimeInterval(startDate, endDate){
+    var url = `${this.baseURL}ProcessTime/${startDate}/${endDate}`;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false );
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.response)['metricValue'];
+  }
+
+  activeIssuesWithColumnId(id){
+    var url = `${this.baseURL}NbActiveIssues/${id}`;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false );
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.response);
+  }
+
+  completedIssuesWithTimeInterval(startDate, endDate){
+    var url = `${this.baseURL}NbCompletedIssues/${startDate}/${endDate}`;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false );
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.response)['metricValue'];
+  }
+
+  issuesForEachColumnWithTimeInterval(startDate, endDate){
+    var url = `${this.baseURL}NbIssues/${startDate}/${endDate}`;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false );
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.response);
+  }
+
+  openPullRequest() {
+    var url = `${this.baseURL}NbOpenPullRequest`;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false );
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.response)['metricValue'];
+  }
+
+  commits() {
+    var url = `${this.baseURL}NbCommits`;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false );
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.response)['metricValue'];
   }
 }
