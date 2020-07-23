3691ea9d8b56c99d6d6a1700b1bc9fe1f4e96ce3
Simon <pelletier.simon.18@gmail.com>
2020-07-23 10:21:23 -0400

    Getting endpoints

--- app/routes/main.js
+++ app/routes/main.js
@@ -3,5 +3,15 @@ import Route from '@ember/routing/route';
 export default class MainRoute extends Route {
   model(){
     this.set('defaultDate', 'test');
+    this.set('test7', this.processTimeWithWithIssue(4601));
+  }
+
+  processTimeWithWithIssue(id){
+    var url = `https://localhost:44319/metric/ProcessTime/${id}`;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false );
+    xmlHttp.send( null );
+    console.log(xmlHttp.responseText);
+    return xmlHttp.responseText;
   }
 }
